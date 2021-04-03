9dd37c0a7dbeb484a4cc5a2ff9c076f697861fe3
arifaysol <ari@tma.com.vn>
2021-04-03 18:07:55 +0700

    Adding EF core to communicate with db

---
+++ Export.UnitTest/Database/MyDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Database.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Database
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext() : base()
+        {
+
+        }
+
+        public DbSet<idaxScenarios> IdaxScenarios { get; set; }
+        public DbSet<IDAX_SCENARIOS> IDAX_SCENARIOS { get; set; }
+        public DbSet<idaxSettings> IdaxSettings { get; set; }
+        public DbSet<IDAX_SETTINGS> IDAX_SETTINGS { get; set; }
+    }
+}
