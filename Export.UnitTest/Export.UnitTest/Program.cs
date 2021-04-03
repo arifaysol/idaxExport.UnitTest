d5cd65ce7efde9b3aa3d3ee0fdcdc1526eac58ef
arifaysol <ari@tma.com.vn>
2021-04-03 17:17:48 +0700

    Initialize nunit test project

---
+++ Export.UnitTest/Export.UnitTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Export.UnitTest
+{
+    class Program
+    {
+        //private static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Hello World!");
+        //}
+    }
+}
---
+++ Export.UnitTest/Export.UnitTest/Test1.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Export.UnitTest
+{
+    [TestFixture]
+    public class Test1
+    {
+        [TestCase]
+        public void TestLogin()
+        {
+            Assert.AreEqual(1, 1);
+        }
+        [TestCase]
+        public void getuserdetails()
+        {
+            Assert.Pass();
+        }
+    }
+}
